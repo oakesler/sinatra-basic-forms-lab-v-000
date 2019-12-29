a296e50c9dbed3f29fbd1d980465f9e84c9120ac
Olivia Kesler <oakesler@protonmail.ch>
2019-12-29 02:41:34 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require_relative 'config/environment'
+require_relative 'models/puppmaker.rb'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    erb :index
+  end
 
+  post '/puppy' do
+    
+  
+   
+  end
+  
+  post '/display_puppy'
+  end
+
+end
 end
