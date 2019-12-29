0567f396def946ea6071965def2a9c13b6e6310e
Olivia Kesler <oakesler@protonmail.ch>
2019-12-29 03:52:03 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,12 +7,12 @@ class App < Sinatra::Base
     erb :index
   end
 
-  get '/create_puppy' do
+  get '/new' do
     erb :create_puppy
   end
   
-  post '/display_puppy' do 
-    @puppy = Puppy.new(params["name"], params["age"], params["breed"])
+  post '/puppy' do 
+    @puppy = Puppy.new(params["name"], params["breed"], params["age"])
     
     erb :display_puppy
   end
