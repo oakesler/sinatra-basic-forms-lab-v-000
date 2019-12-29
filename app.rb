fa70d592c1105b889c42f0435e8c1d7d4b51820b
Olivia Kesler <oakesler@protonmail.ch>
2019-12-29 03:49:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
   
-  post '/display_puppy' do 
+  post '/puppy' do 
     @puppy = Puppy.new(params["name"], params["age"], params["breed"])
     
     erb :display_puppy
